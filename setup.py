28db5a705351fe2903bfa96363fcc6a2dd0e0c0b
tanbr <RealTanbro@Gmail.com>
2016-12-13 14:21:29 +0800

    fix: version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ else:
 
 setup(
     name='exosip2ctypes',
-    version='0.1.1',
+    version='0.1.1.post161213',
     install_requires=INSTALL_REQUIRES,
     packages=find_packages('src'),  # include all packages under src, or special packages in a list.
     package_dir={'': 'src'},  # tell distutils packages are under src
